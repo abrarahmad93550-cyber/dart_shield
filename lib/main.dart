b135ce7c5e4870bd4cc1fdfbdf6dfa5e7317b24d
Your Name <your_email@gmail.com>
2026-07-16 01:56:56 +0500

    added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(const MaterialApp(home: ShieldScreen()));
-
-class ShieldScreen extends StatefulWidget {
-  const ShieldScreen({super.key});
-  @override
-  _ShieldScreenState createState() => _ShieldScreenState();
+void main() {
+  runApp(MaterialApp(home: FirstPage()));
 }
 
-class _ShieldScreenState extends State<ShieldScreen> {
-  bool isProtected = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadStatus(); // ایپ شروع ہوتے ہی پرانا سٹیٹس لوڈ کرے
-  }
-
-  // سٹیٹس سیو کرنے کا فنکشن
-  _toggleShield() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      isProtected = !isProtected;
-    });
-    prefs.setBool('isProtected', isProtected);
-  }
-
-  // سٹیٹس لوڈ کرنے کا فنکشن
-  _loadStatus() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      isProtected = prefs.getBool('isProtected') ?? false;
-    });
-  }
-
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: _toggleShield,
-              child: Icon(
-                Icons.shield,
-                size: 150,
-                color: isProtected ? Colors.green : Colors.grey[400],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              isProtected ? "حفاظت فعال ہے" : "حفاظت غیر فعال ہے",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: isProtected ? Colors.green : Colors.grey,
-              ),
+            Text("حفاظت فعال ہے"),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+              child: Text("اگلا صفحہ"),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("دوسرا صفحہ")),
+      body: Center(child: Text("آپ دوسرے صفحے پر آ گئے ہیں!")),
     );
   }
 }
