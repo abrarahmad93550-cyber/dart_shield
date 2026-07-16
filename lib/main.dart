d62ca0362c3752127f0c86cced6c72c538fae435
Your Name <your_email@gmail.com>
2026-07-16 07:30:31 +0500

    added filter logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(home: FirstPage()));
+void main() => runApp(MaterialApp(home: FilterPage()));
+
+class FilterPage extends StatefulWidget {
+  @override
+  _FilterPageState createState() => _FilterPageState();
 }
 
-class FirstPage extends StatelessWidget {
+class _FilterPageState extends State<FilterPage> {
+  final TextEditingController _controller = TextEditingController();
+  String _result = "";
+
+  void _checkContent() {
+    String text = _controller.text.toLowerCase();
+    // یہاں وہ الفاظ لکھیں جنہیں آپ بلاک کرنا چاہتے ہیں
+    List<String> blockedKeywords = ["گندا", "badword1", "فحش"];
+
+    bool containsBlocked = false;
+    for (String word in blockedKeywords) {
+      if (text.contains(word)) {
+        containsBlocked = true;
+        break;
+      }
+    }
+
+    setState(() {
+      _result = containsBlocked ? "خبردار! یہ مواد ممنوعہ ہے." : "یہ مواد محفوظ ہے.";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(title: Text("فلٹر ایپ")),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("حفاظت فعال ہے"),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondPage()),
-                );
-              },
-              child: Text("اگلا صفحہ"),
+            TextField(
+              controller: _controller, 
+              decoration: InputDecoration(hintText: "یہاں لفظ لکھیں")
             ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _checkContent, 
+              child: Text("چیک کریں")
+            ),
+            SizedBox(height: 20),
+            Text(_result, style: TextStyle(fontSize: 20, color: Colors.red)),
           ],
         ),
       ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("دوسرا صفحہ")),
-      body: Center(child: Text("آپ دوسرے صفحے پر آ گئے ہیں!")),
     );
   }
 }
